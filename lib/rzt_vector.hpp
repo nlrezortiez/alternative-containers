534997c48d6350c66df0a9e337ee9b5f5253f255
Daniil Gerasimenko <newliferezort@gmail.com>
2023-03-17 03:14:49 +0300

    member function realisation & start of implementing allocator awareness

--- lib/rzt_vector.hpp
+++ lib/rzt_vector.hpp
@@ -1,19 +1,30 @@
 #pragma once
 
+#include <cstddef>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+
+// TODO: implement random_access_iterator
+//       implement class-member funcs
+//       must satisfy AllocatorAwareContainer Policy
+
 namespace rzt {
-    template <typename T, typename Alloc = std::allocator<T>>
+    template <typename T, typename Allocator = std::allocator<T>>
     class vector {
     public:
-        using this_type = vector<T, Alloc>;
-        using value_type = typename Alloc::value_type;
-        using pointer = Alloc::value_type*;
-        using const_pointer = const Alloc::value_type*;
-        using reference = Alloc::value_type&;
-        using const_reference = const Alloc::value_type&;
-        using rvalue = Alloc::value_type&&;
+        typedef vector<T, Allocator> this_type;
+        typedef T value_type;
+        typedef T* pointer;
+        typedef const T* const_pointer;
+        typedef T& reference;
+        typedef const T& const_reference;
+        typedef T&& rvalue;
 
-        using difference_type = typename Alloc::difference_type;
-        using size_t = typename Alloc::size_type;
+        typedef typename Allocator::difference_type difference_type;
+        typedef typename Allocator::size_type size_type;
+        typedef Allocator allocator_type;
+        typedef std::allocator_traits<allocator_type> allocator_traits;
 
         template <bool ISConst>
         struct common_iterator {
@@ -42,10 +53,19 @@ namespace rzt {
             std::conditional_t<ISConst, const_pointer, pointer> ptr_;
         };
 
-        using iterator = common_iterator<false>;
-        using const_iterator = common_iterator<true>;
-        using reverse_iterator = std::reverse_iterator<iterator>;
-        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+        typedef common_iterator<false> iterator;
+        typedef common_iterator<true> const_iterator;
+        typedef std::reverse_iterator<iterator> reverse_iterator;
+        typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+        vector() : data_start_(), data_end_(), storage_end_() {
+        }
+        explicit vector(size_type n, const T& value = T(), const Allocator& alloc = Allocator()) : vector() {
+            resize(n, value);
+        }
+
+        vector(const vector& other);
+        vector(const vector& other, const Allocator& alloc);
 
         iterator begin();
         iterator end();
@@ -59,90 +79,185 @@ namespace rzt {
         const_reverse_iterator crend() const;
 
         [[nodiscard]] bool empty() const;
-        size_t size() const;
+        size_type size() const;
         void reserve(size_t new_capacity);
-        size_t capacity() const;
+        size_type capacity() const;
+
+        reference operator[](size_type index);
+        const_reference operator[](size_type index) const;
+        reference at(size_t index);
+        const_reference at(size_t index) const;
 
         void clear();
         void insert();
         void emplace();
         void erase();
         void push_back(const_reference value); // T -> CopyInsertable && MoveInsertable
-        void push_back(rvalue value);
+        //void push_back(rvalue value);
 
         template <typename... Args>
         reference emplace_back(Args&&... args);
 
         void pop_back();
-        void resize(size_t count);
-        void resize(size_t count, const_reference value);
-
-        reference at(size_t pos);
-        const_reference at(size_t) const;
+        void resize(size_t n, const T& value = T());
 
     private:
         pointer data_start_;
         pointer data_end_;
         pointer storage_end_;
+
+        Allocator allocator;
     };
-    template <typename T, typename Alloc>
-    size_t vector<T, Alloc>::size() const {
-        return std::distance(begin(), end());
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::size_type vector<T, Allocator>::size() const {
+        return data_end_ - data_start_;
     }
 
-    template <typename T, typename Alloc>
-    bool vector<T, Alloc>::empty() const {
-        return size() == 0;
+    template <typename T, typename Allocator>
+    bool vector<T, Allocator>::empty() const {
+        return begin() == end();
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::iterator vector<T, Alloc>::begin() {
-        return rzt::vector<T, Alloc>::iterator(data_start_);
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::size_type vector<T, Allocator>::capacity() const {
+        return storage_end_ - data_start_;
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::iterator vector<T, Alloc>::end() {
-        return rzt::vector<T, Alloc>::iterator(data_end_);
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::const_reference vector<T, Allocator>::at(size_t index) const {
+       if (index > size()) throw std::out_of_range("out_of_range");
+       return *(data_start_ + index);
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::const_iterator vector<T, Alloc>::begin() const {
-        return rzt::vector<T, Alloc>::const_iterator(data_start_);
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::reference vector<T, Allocator>::at(size_t index) {
+        if (index > size()) throw std::out_of_range("out_of_range");
+        return *(data_start_ + index);
+    }
+    
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::reference vector<T, Allocator>::operator[](size_type index) {
+        return *(data_start_ + index);
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::const_iterator vector<T, Alloc>::end() const {
-        return rzt::vector<T, Alloc>::const_iterator(data_end_);
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::const_reference vector<T, Allocator>::operator[](size_type index) const {
+        return *(data_start_ + index);
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::const_iterator vector<T, Alloc>::cbegin() const {
-        return rzt::vector<T, Alloc>::const_iterator(data_start_);
+    template <typename T, typename Allocator>
+    void vector<T, Allocator>::reserve(size_t new_capacity) {
+        if (new_capacity < size())
+            return;
+
+        T* new_storage = allocator_traits::allocate(allocator, new_capacity); // possible exception
+        size_type i = 0;
+        try {
+            for (; i < new_capacity; ++i) {
+                allocator_traits::construct(allocator, new_storage + i, T());
+            }
+        } catch (...) {
+            for (size_t j = 0; j < i; ++j) {
+                allocator_traits::destroy(allocator, new_storage + j);
+            }
+            allocator_traits::deallocate(allocator, new_storage, new_capacity);
+            throw;
+        }
+        for (i = 0; i < size(); ++i) {
+            allocator_traits::destroy(allocator, data_start_ + i);
+        }
+        allocator_traits::deallocate(allocator, data_start_, i); // i == size();
+        data_start_ = new_storage;
+        if (new_capacity == 1) data_end_ = data_start_;
+        storage_end_ = data_start_ + new_capacity;
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::const_iterator vector<T, Alloc>::cend() const {
-        return rzt::vector<T, Alloc>::const_iterator(data_end_);
+    template <typename T, typename Allocator>
+    void vector<T, Allocator>::push_back(const T& value) {
+        if (data_end_ == storage_end_) {
+            reserve(capacity() > 0 ? 2 * capacity() : 1);
+        }
+        allocator_traits::construct(allocator, data_end_++, value);
+
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::reverse_iterator vector<T, Alloc>::rbegin() {
-        return rzt::vector<T, Alloc>::reverse_iterator(data_end_);
+    template <typename T, typename Allocator>
+    void vector<T, Allocator>::pop_back() {
+        allocator_traits::destroy(allocator, data_end_--);
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::reverse_iterator vector<T, Alloc>::rend() {
-        return rzt::vector<T, Alloc>::reverse_iterator(data_start_);
+    template <typename T, typename Allocator>
+    void vector<T, Allocator>::resize(std::size_t n, const T& value) {
+        if (n > capacity()) {
+            reserve(n);
+            for (int i = size(); i < n; ++i) {
+                allocator_traits::construct(allocator, data_start_ + i, value);
+            }
+            storage_end_ = data_end_ = data_start_ + n;
+        } else {
+            if (n > size()) {
+                for (int i = size(); i < n; ++i) {
+                    allocator_traits::construct(allocator, data_start_ + i, value);
+                }
+                data_end_ = data_start_ + n;
+            } else {
+                for (int i = n; i < size(); ++i) {
+                    allocator_traits::destroy(allocator, data_start_ + i);
+                }
+                data_end_ = data_start_ + n;
+            }
+        }
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::const_reverse_iterator vector<T, Alloc>::crbegin() const {
-        return rzt::vector<T, Alloc>::const_reverse_iterator(data_end_);
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::iterator vector<T, Allocator>::begin() {
+        return rzt::vector<T, Allocator>::iterator(data_start_);
     }
 
-    template <typename T, typename Alloc>
-    vector<T, Alloc>::const_reverse_iterator vector<T, Alloc>::crend() const {
-        return rzt::vector<T, Alloc>::const_reverse_iterator(data_start_);
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::iterator vector<T, Allocator>::end() {
+        return rzt::vector<T, Allocator>::iterator(data_end_);
+    }
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::const_iterator vector<T, Allocator>::begin() const {
+        return rzt::vector<T, Allocator>::const_iterator(data_start_);
+    }
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::const_iterator vector<T, Allocator>::end() const {
+        return rzt::vector<T, Allocator>::const_iterator(data_end_);
+    }
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::const_iterator vector<T, Allocator>::cbegin() const {
+        return rzt::vector<T, Allocator>::const_iterator(data_start_);
+    }
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::const_iterator vector<T, Allocator>::cend() const {
+        return rzt::vector<T, Allocator>::const_iterator(data_end_);
+    }
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::reverse_iterator vector<T, Allocator>::rbegin() {
+        return rzt::vector<T, Allocator>::reverse_iterator(data_end_);
+    }
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::reverse_iterator vector<T, Allocator>::rend() {
+        return rzt::vector<T, Allocator>::reverse_iterator(data_start_);
+    }
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::const_reverse_iterator vector<T, Allocator>::crbegin() const {
+        return rzt::vector<T, Allocator>::const_reverse_iterator(data_end_);
+    }
+
+    template <typename T, typename Allocator>
+    typename vector<T, Allocator>::const_reverse_iterator vector<T, Allocator>::crend() const {
+        return rzt::vector<T, Allocator>::const_reverse_iterator(data_start_);
     }
 
 } // namespace rzt
