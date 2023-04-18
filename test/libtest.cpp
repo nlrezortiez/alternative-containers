b852b138469fe752ea668be1ad2f5e136e83458f
nlrezortiez <newliferezort@gmail.com>
2023-04-18 11:05:55 +0300

    test commit

--- test/libtest.cpp
+++ test/libtest.cpp
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 #include <iostream>
 #include <iterator>
@@ -10,22 +11,29 @@
 
 #include <gtest/gtest.h>
 
-template <typename T>
-void sandbox() {
-    rzt::vector<T> v1;
-    v1.push_back("index0");
-    v1.push_back("index1");
-    v1.push_back("index2");
-    v1.push_back("index3");
-    v1.insert(v1.begin() + 2, "inserted");
-    for (const auto& string : v1) {
-        std::cout << string << std::endl;
-    }
+struct simple_struct {
+    simple_struct() = default;
+    simple_struct(int a, double b, std::string c) : a(a), b(b), c(c) {}
+
+    int a;
+    double b;
+    std::string c;
+};
+
+std::ostream& operator<<(std::ostream& out, simple_struct item) {
+    out << item.a << " " << item.b << " " << item.c;
+    return out;
 }
 
-int main(int argc, char *argv[]) {
-    sandbox<std::string>();
-    //::testing::InitGoogleTest(&argc, argv);
-    //return RUN_ALL_TESTS();
+template <typename T>
+void sandbox(rzt::vector<std::string>& pvector) {
+    rzt::vector<simple_struct> struct_vec;
+    struct_vec.emplace_back(1, 2.3, "qwe");
+    std::cout << struct_vec[0];
+}
+
+int main(int argc, char* argv[]) {
+    rzt::vector<std::string> vec(10, "wtf");
+    sandbox<std::string>(vec);
     return 0;
 }
